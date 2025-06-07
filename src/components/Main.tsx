3d78d2dc7b6e218c5c378ae6f257d3205f762af1
ssgaryss <745008538@qq.com>
2025-06-08 01:17:23 +0800

    Set up the template.

---
+++ src/components/Main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+
+function Main() {
+
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="image-wrapper">
+          <img src={process.env.PUBLIC_URL + "/profile.png"} alt="Avatar" />
+        </div>
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/ssgaryss" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+          <h1>Huidi Qiu</h1>
+          <p>This is Gary's personal website</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/ssgaryss" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
